13489eae847b173429a2244bf88b7e03f6d62661
Shailendra-Java <shailendrasingh2011@live.in>
2018-01-17 12:12:14 +0530

    e-mart

--- e-mart/src/main/java/com/controller/IndexController.java
+++ e-mart/src/main/java/com/controller/IndexController.java
@@ -1,16 +1,15 @@
 package com.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+@Controller
+public class IndexController {
 
-	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping("/index")
+	public ModelAndView modelView()
+	{
+		return new ModelAndView("index","msg","Hello");
 	}
-
 }
