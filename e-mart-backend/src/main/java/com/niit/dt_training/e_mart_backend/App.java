7e5e270bec1aa3586c393f8e48ded9f7f8bf88fd
Shailendra-Java <shailendrasingh2011@live.in>
2018-01-19 17:49:45 +0530

    e-mart

--- e-mart-backend/src/main/java/com/niit/dt_training/e_mart_backend/App.java
+++ e-mart-backend/src/main/java/com/niit/dt_training/e_mart_backend/App.java
@@ -2,6 +2,11 @@ package com.niit.dt_training.e_mart_backend;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 import com.dt.dao.UserDao;
 import com.dt.daoimpl.UserDaoImpl;
 import com.dt.model.User;
@@ -14,6 +19,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Configuration cfg = new Configuration();
+		cfg.configure("com/dt/config/hibernate.cfg.xml");
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction t = session.beginTransaction();
+		
         System.out.println( "User Information" );
         UserDao us = new UserDaoImpl();
         List<User> user = us.getUsers();
